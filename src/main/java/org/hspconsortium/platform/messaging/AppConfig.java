ab767b69ec59435712fe9fc94d830ec8cb9a8c61
Travis Cummings <travis@istalusconsulting.com>
2015-09-30 13:55:05 -0700

    Removed dead code

--- src/main/java/org/hspconsortium/platform/messaging/AppConfig.java
+++ src/main/java/org/hspconsortium/platform/messaging/AppConfig.java
@@ -19,33 +19,7 @@ public class AppConfig {
 
     @Bean
     public KnowledgeBase knowledgeBase() {
-        // load known rules
-//        ClassPathResource classPathResource =
-//                new ClassPathResource("/org/hspconsortium/platform/messaging/BilirubinObservation.drl");
-//
-//        KnowledgeBuilder kbuilder = null;
-//        try {
-//            kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
-//            kbuilder.add(
-//                    ResourceFactory.newInputStreamResource(classPathResource.getInputStream()),
-//                    ResourceType.DRL);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        KnowledgeBuilderErrors errors = kbuilder.getErrors();
-//
-//        if (errors.size() > 0) {
-//            for (KnowledgeBuilderError error : errors) {
-//                System.err.println(error);
-//            }
-//            throw new IllegalArgumentException("Could not parse knowledge.");
-//        }
-
-        KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
-//        knowledgeBase.addKnowledgePackages(kbuilder.getKnowledgePackages());
-
-        return knowledgeBase;
+        return KnowledgeBaseFactory.newKnowledgeBase();
     }
 
 }
--- src/main/java/org/hspconsortium/platform/messaging/Application.java
+++ src/main/java/org/hspconsortium/platform/messaging/Application.java
@@ -18,13 +18,6 @@ public class Application extends SpringBootServletInitializer {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("Welcome to Spring Boot with context: " + ctx);
-//        System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//        String[] beanNames = ctx.getBeanDefinitionNames();
-//        Arrays.sort(beanNames);
-//        for (String beanName : beanNames) {
-//            System.out.println(beanName);
-//        }
     }
 
 }
