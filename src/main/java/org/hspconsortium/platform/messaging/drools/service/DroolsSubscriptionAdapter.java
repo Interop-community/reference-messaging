de46d7ec1fe6d9a3d7dea960a6ab6f4de57be4a6
Travis Cummings <travis@istalusconsulting.com>
2015-12-22 12:48:27 -0700

    HRI-465 Initial support of IdToken validation

--- src/main/java/org/hspconsortium/platform/messaging/drools/service/DroolsSubscriptionAdapter.java
+++ src/main/java/org/hspconsortium/platform/messaging/drools/service/DroolsSubscriptionAdapter.java
@@ -4,8 +4,6 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.api.IResource;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hspconsortium.platform.messaging.service.SubscriptionManagerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
