ea0253960596c2d1efb4acca8ac2b0df8aa7d77d
Travis <travis@Travis-MacBook-Pro.local>
2017-04-10 22:30:49 -0600

    Release configuration

--- reference-messaging/src/main/java/org/hspconsortium/platform/messaging/Application.java
+++ reference-messaging/src/main/java/org/hspconsortium/platform/messaging/Application.java
@@ -3,7 +3,7 @@ package org.hspconsortium.platform.messaging;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.integration.config.EnableIntegration;
 
